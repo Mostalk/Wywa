6b15c94c9e5c8928a1d5777901c49a032d024cba
Mostalk <ivdifa.ka@gmail.com>
2020-04-20 13:17:31 +0300

    New files

--- main.py
+++ main.py
@@ -1,40 +1,23 @@
 import os
-import re
-from threading import Thread
-
 import cv2
-import jamspell
 import numpy as np
 import math
 import operator
 import test
-import proc as p
-from matplotlib import pyplot as plt
 from autocorrect import Speller
+from threading import Thread
 
 corrector = Speller(lang='ru')
 
 
 class SrcThread(Thread):
     def __init__(self, image):
-        """Инициализация потока"""
         Thread.__init__(self)
         self.image = image
 
     def run(self):
         image_src = "checks/" + self.image
         image = cv2.imread(image_src)
-
-        def resize(image):
-            try:
-                scale_percent = 20
-                width = int(image.shape[1] * scale_percent / 100)
-                height = int(image.shape[0] * scale_percent / 100)
-                dim = (width, height)
-                image1 = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-                return image1, dim
-            except:
-                return image, dim
 
         def morf(image):
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -52,40 +35,33 @@ class SrcThread(Thread):
         def processImage(image):
             print("Start processing image...")
             cnts, hier = cv2.findContours(morf(image)[3].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            total = 0
             li = {}
             i = 0
             for c in cnts:
                 peri = cv2.arcLength(c, True)
                 approx = cv2.approxPolyDP(c, 0.0025 * peri, False)
-                rect = cv2.minAreaRect(approx)  # пытаемся вписать прямоугольник
-                area = int(rect[1][0] * rect[1][1])  # вычисление площади
+                rect = cv2.minAreaRect(approx)
+                area = int(rect[1][0] * rect[1][1])
                 li.update({i: area})
                 i = i + 1
                 ki = sorted(li.items(), key=operator.itemgetter(1))
-                # cv2.drawContours(a_thresh, [c], 0, (0, 0, 255), 10)
             c = cnts[ki[-1][0]]
             peri = cv2.arcLength(c, True)
             approx = cv2.approxPolyDP(c, 0.0025 * peri, False)
-            rect = cv2.minAreaRect(approx)  # пытаемся вписать прямоугольник
-            box = cv2.boxPoints(rect)  # поиск четырех вершин прямоугольника
-            box = np.int0(box)  # округление координат
-            cv2.drawContours(image, [box], 0, (0, 0, 255), 10)  # рисуем прямоугольник
+            rect = cv2.minAreaRect(approx)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(image, [box], 0, (0, 0, 255), 10)
             morf(image)[-1]
             image = a_thresh
-            # center = (int(rect[0][0]), int(rect[0][1]))
-            # вычисление координат двух векторов, являющихся сторонам прямоугольника
             edge1 = np.int0((box[1][0] - box[0][0], box[1][1] - box[0][1]))
             edge2 = np.int0((box[2][0] - box[1][0], box[2][1] - box[1][1]))
-            # выясняем какой вектор больше
             usedEdge = edge1
             if cv2.norm(edge2) > cv2.norm(edge1):
                 usedEdge = edge2
-            reference = (1, 0)  # горизонтальный вектор, задающий горизонт
-            # вычисляем угол между самой длинной стороной прямоугольника и горизонтом
-            angle = 180.0 / math.pi * math.acos((reference[0] * usedEdge[0] + reference[1] * usedEdge[1]) / (cv2.norm(reference) * cv2.norm(usedEdge)))
-            # cv2.drawContours(image, cnts, 0, (50, 0, 255), 5)
-            # выводим в кадр величину угла наклона
+            reference = (1, 0)
+            angle = 180.0 / math.pi * math.acos(
+                (reference[0] * usedEdge[0] + reference[1] * usedEdge[1]) / (cv2.norm(reference) * cv2.norm(usedEdge)))
             (h, w) = image.shape[:2]
             center_pic = (w / 2, h / 2)
             M = cv2.getRotationMatrix2D(center_pic, 90 - angle, 1.0)
@@ -95,21 +71,12 @@ class SrcThread(Thread):
             ww = int(box[1][0])
             wy = int(box[3][0])
             cut = image[yy:yw, ww:wy]
-            k = str(re.sub("checks/", "", image_src))
-            '''cv2.imshow(k, resize(cut)[0])
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()'''
-
-            # cv2.imshow("2", edged)
-            # cv2.imshow("3", threshold_image)
-            print("Processing complete!")
 
             return cut
 
         image, gray, edged, closed, thresh, a_thresh = morf(image)
-        # processImage(image)
         test.create_threads(processImage(image), image_src)
-        #test.teser(processImage(gray, edged, closed))
+
 
 def create_threads():
     files = os.listdir("checks/")
--- test.py
+++ test.py
@@ -1,17 +1,7 @@
-import re
-import cv2
-import numpy as np
 import pytesseract
-
-import main
-import proc as p
-import jamspell
 from pytesseract import Output
-from matplotlib import pyplot as plt
 from threading import Thread
 import re
-
-from main import corrector
 
 
 class MyThread(Thread):
@@ -23,139 +13,21 @@ class MyThread(Thread):
         self.src = image_src
 
     def run(self):
-        def processing(text):
-            summ = ["СУМА", "СУМ", "СУММА", "СЕМА"]
-            for sum in summ:
-                if sum in text['text']:
-                    su = open("log/sum.txt", 'a')
-                    ii = 1
-                    while text['text'][sum.index(sum) + ii] == "":
-                        ii = ii + 1
-                        print(sum.index(sum) + ii)
-                    su.write(text['text'][sum.index(sum) + ii])
-                    su.close()
-
         def teser(image):
-            dd = []
-            # print("Recognition...")
             psm = self.name
-            # print("Mode: " + psm + " start")
             f.write("====================" + re.sub("checks/", "", self.src) + "====================\n")
-            print("====================" + re.sub("checks/", "", self.src) + "====================\n")
             pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-            modes = ['get_grayscale', 'remove_noise', 'thresholding', 'dilate', 'erode', 'opening', 'canny']
-            # print("--------" + "CLEAR" +"-------- " + str(psm) + " --------")
-            d = pytesseract.image_to_data(image, lang='rus+ukr', output_type=Output.STRING, config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            cv2.drawContours(image, (d['width'], d['height']), 0, (0, 0, 255), 10)
-            cv2.imshow(self.src, main.resize(image)[0])
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-            # f.write("--------" + "CLEAR" + "--------\n")
-            # dd = d.split(" ")
-            print(d)
-            for word in d['text']:
-                try:
-                    # d = corrector(word)
-                    None
-                except:
-                    continue
-            for text in d['text']:
-                if not text == "":
-                    f.write(str(d['text']))
-            f.write("----------------\n")
-            '''# print("--------" + modes[0] + "-------- " + str(psm) + " --------")
-            gray = p.get_grayscale(image)
-            d = pytesseract.image_to_string(gray, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[0] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[1] + "-------- " + str(psm) + " --------")
-            noice = p.remove_noise(gray)
-            d = pytesseract.image_to_string(noice, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[1] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[2] + "-------- " + str(psm) + " --------")
-            thresh = p.thresholding(gray)
-            d = pytesseract.image_to_string(thresh, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[2] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[3] + "-------- " + str(psm) + " --------")
-            dilate = p.dilate(gray)
-            d = pytesseract.image_to_string(dilate, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[3] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[4] + "-------- " + str(psm) + " --------")
-            erode = p.erode(gray)
-            d = pytesseract.image_to_string(erode, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[4] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[5] + "-------- " + str(psm) + " --------")
-            opening = p.opening(gray)
-            d = pytesseract.image_to_string(opening, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[5] + "--------\n")
-            f.write(d)
-            f.write("----------------\n")
-            # print("--------" + modes[6] + "-------- " + str(psm) + " --------")
-            canny = p.canny(gray)
-            d = pytesseract.image_to_string(canny, lang='rus+ukr', config="--psm " + str(
-                psm))  # d = pytesseract.image_to_data(image, lang='rus', output_type=Output.DICT, config="--psm" + psm")
-            # print(d)
-            f.write("--------" + modes[6] + "--------\n")
-            f.write(d)'''
-
-            print("Mode: " + psm + " end!")
+            d = pytesseract.image_to_data(image, lang='rus+ukr', output_type=Output.STRING, config="--psm " + str(psm))
             return d
-
         f = open("log/recognition.txt", "a")
         teser(self.image)
         f.close()
-
-    """  #n_boxes = len(d['text'])
-    
-    linex = 0
-    #for i in range(n_boxes):
-    #    line = []
-        # condition to only pick boxes with a confidence > 60%
-#            if int(d['conf'][i]) > 10:
-#                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-#                image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-#               line.append(d['text'][i])
-#           #print(line)
-    #b, g, r = cv2.split(image)
-    #rgb_img = cv2.merge([r, g, b])
-    #plt.figure(figsize=(5, 5))
-    scale_percent = 100  # Процент от изначального размера
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    dim = (width, height)
-    numb = range(1,1000)
-    image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-    cv2.imshow("test" + str(numb),image)
-    #plt.title('Пример')
-    #plt.show()"""
 
 
 def create_threads(image, image_src):
     name = 6
     my_thread = MyThread(name, image, image_src)
-        my_thread.start()
+    my_thread.start()
 
 
 if __name__ == "__main__":
